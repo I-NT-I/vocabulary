c8ec532275c18b14bf2401cfb7c2f72f184f9092
Nico <l-Nt-l@protonmail.com>
2024-01-18 22:49:57 +0100

    [TASK] add styling clear function

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ var GuessCount = 0
 
 function addVoc(){
     
-    // Vocinput[Count][0] = document.getElementById('InputVoc1').value ;
-    Vocinput.push([document.getElementById('InputVoc1').value, document.getElementById('InputVoc2').value])
-    // Vocinput[Count][1] = document.getElementById('InputVoc2').value ;
 
-    if (Vocinput[Count][1] != "" && Vocinput[Count][0]) {
+
+    if (document.getElementById('InputVoc1').value != "" && document.getElementById('InputVoc2').value != "") { // check if input is empty 
+        Vocinput.push([document.getElementById('InputVoc1').value, document.getElementById('InputVoc2').value]) // add voc in array 
+
         console.log(Vocinput)
         Count++
         $("#Anzahl").text(Vocinput.length);
@@ -27,27 +27,30 @@ function addVoc(){
 }
 function guess(){
 
-    // document.getElementById('Word1').value = Vocinput[CounterVoc][0];
 
 
     if(document.getElementById('Guess1').value == Vocinput[GuessCount][1] ){
         $(".Check").text("Right!");
         GuessCount++
         $("#NextWordBtn").prop("disabled",false)
-        
+        $("#checkwordbtn").prop("disabled",true)
     }
     else{
         $(".Check").text("Wrong!");
 
     }
 
-    //document.getElementById('Guess1')
 }
 function addword(){
-    
+    if($(".Check").text == ""){
+        alert("Box empty");
+    }
     document.getElementById('Word1').value = Vocinput[GuessCount][0];
     console.log( document.getElementById('Word1').value)
     console.log( Vocinput[GuessCount][0])
     $("#NextWordBtn").prop("disabled",true)
+    $(".Check").text("");
+    $("#Guess1").val('');
+    $("#checkwordbtn").prop("disabled",false)
 
 }
